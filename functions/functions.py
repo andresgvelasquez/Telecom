3cb2eafe15549414dc660c34221657edf3c7435b
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-02-13 19:34:13 -0800

    Se agrego la función para dar formato snake_case al nombre de las columnas.

---
+++ functions/functions.py
@@ -0,0 +1,13 @@
+def camelcase_to_snakecase(df):
+    ''' Convierte las columnas de un dataframe de formato CamelCase
+    a snake_case y minúsculas.
+    in: dataframe con columnas CamelCase
+    out: dataframe con columnas snake_case '''
+    columnas = df.columns
+    new_col_names = []
+    for col in columnas:
+        pattern = r'([a-z])([A-Z])'
+        replace = r'\1_\2'
+        snke_case_name = re.sub(pattern=pattern, repl=replace, string=col)
+        new_col_names.append(snke_case_name.lower())
+    df.columns = new_col_names
