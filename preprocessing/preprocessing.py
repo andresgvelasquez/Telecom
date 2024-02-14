b2a53d700039fa6b350ce9a0f58026cd0c557e02
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-02-14 11:39:36 -0800

    Se convirtieron todas columnas con 2 valores categoricos a tipo bool.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 sys.path.append('./functions')
 
-from functions import camelcase_to_snakecase
+from functions import camelcase_to_snakecase, column_to_bool
 
 # Carga de de dataframes
 df_contract = pd.read_csv('./files/datasets/input/contract.csv', parse_dates=['BeginDate'])
@@ -18,8 +18,21 @@ for df in datasets:
 # Convertir la columna end_date a datetime
 df_contract['end_date'] = pd.to_datetime(df_contract['end_date'], errors='coerce')
 
-# Convertir las columans de 2 valores a bool
-df_contract = pd.get_dummies(df_contract, columns=['paperless_billing'], drop_first=True)
+# Convertir las columas de 2 valores a bool
+# Contract
+df_contract = column_to_bool(df_contract, ['paperless_billing'])
+
+#Internet 
+df_internet = column_to_bool(df_internet, ['internet_service', 'online_security', 'online_backup', 'device_protection',
+                                           'tech_support', 'streaming_tv', 'streaming_movies'])
+df_internet = df_internet.rename(columns={'internet_service':'internet_fiber_optic'})
+
+# Personal
+df_personal = column_to_bool(df_personal, ['gender', 'partner', 'dependents'])
+df_personal = df_personal.rename(columns={'gender':'gender_male'}) 
+
+# Phone
+df_phone = column_to_bool(df_phone, ['multiple_lines'])
 
 # Separar la columna begin_date de df_contract en año, mes y día
 df_contract['begin_day'] = df_contract['begin_date'].dt.day
@@ -32,5 +45,5 @@ df_contract['begin_year'] = df_contract['begin_date'].dt.year
 #df_contract['end_month'] = df_contract['end_date'].dt.month
 #df_contract['end_year'] = df_contract['end_date'].dt.year
 
-print(df_contract.head(10))
-print(df_contract.info())
+#print(df_contract.head(5))
+#print(df_contract.info())
