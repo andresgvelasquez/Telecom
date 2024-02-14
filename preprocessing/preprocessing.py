584fe1b1afc80db9b782eebfe15be7bafd1ed6c2
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-02-14 09:55:16 -0800

    Se convirtio la columna end_date a tipo datetime.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -15,9 +15,19 @@ datasets = [df_contract, df_internet, df_personal, df_phone]
 for df in datasets:
     camelcase_to_snakecase(df)
 
+# Convertir la columna end_date a datetime
+df_contract['end_date'] = pd.to_datetime(df_contract['end_date'], errors='coerce')
+
 # Separar la columna begin_date de df_contract en año, mes y día
 df_contract['begin_day'] = df_contract['begin_date'].dt.day
 df_contract['begin_month'] = df_contract['begin_date'].dt.month
 df_contract['begin_year'] = df_contract['begin_date'].dt.year
 
+
+# Separar la columna end_date en año, mes y día
+#df_contract['end_day'] = df_contract['end_date'].dt.day
+#df_contract['end_month'] = df_contract['end_date'].dt.month
+#df_contract['end_year'] = df_contract['end_date'].dt.year
+
 print(df_contract.head(10))
+#print(df_contract.info())
