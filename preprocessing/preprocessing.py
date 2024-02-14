5d632dda4ca3d6f82ee4d9dc6e7a68789a714ae2
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-02-14 12:22:34 -0800

    Se separa el día, mes y año de las columnas de inicio y fin de contract.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 sys.path.append('./functions')
 
-from functions import camelcase_to_snakecase, column_to_bool
+from functions import camelcase_to_snakecase, column_to_bool, split_dates
 
 # Carga de de dataframes
 df_contract = pd.read_csv('./files/datasets/input/contract.csv', parse_dates=['BeginDate'])
@@ -35,15 +35,18 @@ df_personal = df_personal.rename(columns={'gender':'gender_male'})
 df_phone = column_to_bool(df_phone, ['multiple_lines'])
 
 # Separar la columna begin_date de df_contract en año, mes y día
-df_contract['begin_day'] = df_contract['begin_date'].dt.day
-df_contract['begin_month'] = df_contract['begin_date'].dt.month
-df_contract['begin_year'] = df_contract['begin_date'].dt.year
+df_contract = split_dates(df_contract, 'begin_date', 'begin')
 
+# Separar la columna end_date de df_contract en año, mes y día
+df_contract = split_dates(df_contract, 'end_date', 'end')
+
+print(df_contract.head(5))
 
 # Separar la columna end_date en año, mes y día
 #df_contract['end_day'] = df_contract['end_date'].dt.day
 #df_contract['end_month'] = df_contract['end_date'].dt.month
 #df_contract['end_year'] = df_contract['end_date'].dt.year
 
-#print(df_contract.head(5))
+
+#print(df_contract.info())
 #print(df_contract.info())
