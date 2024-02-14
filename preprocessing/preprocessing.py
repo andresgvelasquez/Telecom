05b41bfd4e70ad2e86399d9faf3b7ef6a581ec90
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-02-14 09:47:05 -0800

    Se separo la columna begin_date en nuevas columnas con el día, mes y año.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -5,12 +5,19 @@ sys.path.append('./functions')
 from functions import camelcase_to_snakecase
 
 # Carga de de dataframes
-df_contract = pd.read_csv('./files/datasets/input/contract.csv')
+df_contract = pd.read_csv('./files/datasets/input/contract.csv', parse_dates=['BeginDate'])
 df_internet = pd.read_csv('./files/datasets/input/internet.csv')
 df_personal = pd.read_csv('./files/datasets/input/personal.csv')
 df_phone    = pd.read_csv('./files/datasets/input/phone.csv')
 
-# Darle un formato de minúsculas y snake_case a las columnas
+# Darle un formato de minúsculas y snake_case a las columnas de cada dataframe
 datasets = [df_contract, df_internet, df_personal, df_phone]
 for df in datasets:
     camelcase_to_snakecase(df)
+
+# Separar la columna begin_date de df_contract en año, mes y día
+df_contract['begin_day'] = df_contract['begin_date'].dt.day
+df_contract['begin_month'] = df_contract['begin_date'].dt.month
+df_contract['begin_year'] = df_contract['begin_date'].dt.year
+
+print(df_contract.head(10))
