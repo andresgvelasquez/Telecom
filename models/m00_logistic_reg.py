b825679c7b247c131c7a0318df24cc8280803171
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-02-15 12:10:24 -0800

    roc_auc 0.9489 entrenamiento basico.

--- models/m00_logistic_reg.py
+++ models/m00_logistic_reg.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import roc_auc_score
 # Carga de datos escalados
 features_train = pd.read_csv('./files/datasets/intermediate/features_train_encoded_scaled.csv')
 features_test = pd.read_csv('./files/datasets/intermediate/features_test_encoded_scaled.csv')
 target_train = pd.read_csv('./files/datasets/intermediate/target_train.csv')
 target_test = pd.read_csv('./files/datasets/intermediate/target_test.csv')
 
+# Entrenar el modelo
 model = LogisticRegression()
-
+model.fit(features_train, target_train)
+predicts = model.predict(features_test)
+print(roc_auc_score(target_test, predicts))
